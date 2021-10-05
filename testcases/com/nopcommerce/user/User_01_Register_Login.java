df8d9af03d2967c19a36f332735683423bf1c714
Quang Vuong <duyquang26103@gmail.com>
2021-10-05 22:24:58 +0700

    Unit automation framework

---
+++ testcases/com/nopcommerce/user/User_01_Register_Login.java
@@ -0,0 +1,34 @@
+package com.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class User_01_Register_Login {
+	
+	
+	  @BeforeClass
+	  public void beforeClass() {
+		  
+
+	  }
+	  
+	  @Test
+	  public void TC_01() {
+	
+	  }
+	  
+	  @Test
+	  public void TC_02() {
+	
+	  }
+
+	  @AfterClass
+	  public void afterClass() {
+	
+	  }
+
+}
