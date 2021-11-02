36f34c37804926d87d45cb97060affe0bdd40a42
quang <duyquang26103@gmail.com>
2021-11-02 16:20:28 +0700

    Apply extentReportV2 with screenshot and close Browser function

--- actions/commons/BaseTest.java
+++ actions/commons/BaseTest.java
@@ -1,6 +1,7 @@
 package commons;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
@@ -17,7 +18,7 @@ public class BaseTest {
 	private WebDriver driver;
 	private String projectPath = System.getProperty("user.dir");
 	String emailLogin;
-	private final Log log;
+	protected final Log log;
 	
 	protected BaseTest() {
 		log = LogFactory.getLog(getClass());
@@ -150,7 +151,71 @@ public class BaseTest {
 	protected boolean verifyEquals(Object actual, Object expected) {
 		return checkEquals(actual, expected);
 	}
+
+	public WebDriver getWebDriver() {
+		return this.driver;
+	}
 	
+	protected void closeBrowserAndDriver() {
+		String cmd = "";
+		try {
+			String osName = System.getProperty("os.name").toLowerCase();
+			log.info("OS name = " + osName);
+
+			String driverInstanceName = driver.toString().toLowerCase();
+			log.info("Driver instance name = " + osName);
+
+			if (driverInstanceName.contains("chrome")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq chromedriver*\"";
+				} else {
+					cmd = "pkill chromedriver";
+				}
+			} else if (driverInstanceName.contains("internetexplorer")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq IEDriverServer*\"";
+				}
+			} else if (driverInstanceName.contains("firefox")) {
+				if (osName.contains("windows")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq geckodriver*\"";
+				} else {
+					cmd = "pkill geckodriver";
+				}
+			} else if (driverInstanceName.contains("edge")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq msedgedriver*\"";
+				} else {
+					cmd = "pkill msedgedriver";
+				}
+			} else if (driverInstanceName.contains("opera")) {
+				if (osName.contains("window")) {
+					cmd = "taskkill /F /FI \"IMAGENAME eq operadriver*\"";
+				} else {
+					cmd = "pkill operadriver";
+				}
+			} else if (driverInstanceName.contains("safari")) {
+				if (osName.contains("mac")) {
+					cmd = "pkill safaridriver";
+				}
+			}
+
+			if (driver != null) {
+				driver.manage().deleteAllCookies();
+				driver.quit();
+			}
+		} catch (Exception e) {
+			log.info(e.getMessage());
+		} finally {
+			try {
+				Process process = Runtime.getRuntime().exec(cmd);
+				process.waitFor();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
 	
 	
 
---
+++ testcases/com/nopcommerce/user/Level_13_Register_Login_Log_Report.java
@@ -0,0 +1,95 @@
+package com.nopcommerce.user;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+import commons.BaseTest;
+import pageObjects.nopCommerce.HomePageObject;
+import pageObjects.nopCommerce.LoginPageObject;
+import pageObjects.nopCommerce.MyAccountPageObject;
+import pageObjects.nopCommerce.PageGeneratorManager;
+import pageObjects.nopCommerce.RegisterPageObject;
+
+public class Level_13_Register_Login_Log_Report extends BaseTest {
+	WebDriver driver;
+	String email, password;
+
+	@Parameters({ "browser", "url" })
+	@BeforeClass
+	public void beforeClass(String browserName, String appUrl) {
+		log.info("Pre-Condition - Step 01: Open browser '" + browserName + "' and navigate to '" + appUrl + "'"); 
+		driver = getDriverBrowsers(browserName, appUrl);
+		homePage = PageGeneratorManager.getHomePage(driver);
+
+		email = RandomEmail();
+		password = "123123";
+	}
+
+	@Test
+	public void TC_01_Register_To_System() {
+		log.info("User 01_Register - Step 01: Click to 'Register' Link ");
+		registerPage = homePage.clickToRegisterPage();
+		
+		log.info("User 01_Register - Step 02: Click to Gender Radio Button ");
+		registerPage.clickToGenderRadioButton();
+
+		log.info("User 01_Register - Step 03: Input to 'First Name' textbox:" + "Abner" );
+		registerPage.inputFirstNameTextBox("Abner");
+
+		log.info("User 01_Register - Step 04: Input to 'Last Name' textbox:" + "Siba" );
+		registerPage.inputLastNameTextBox("Siba");
+
+		log.info("User 01_Register - Step 05: Input to 'Email textbox':" + email );
+		registerPage.inputEmailTextBox(email);
+
+		log.info("User 01_Register - Step 06: Input to 'Password' textbox:" + password );
+		registerPage.inputPasswordTextBox(password);
+
+		log.info("User 01_Register - Step 07: Input to 'Confirm Password' textbox:" + password );
+		registerPage.inputConfirmPasswordTextBox(password);
+
+		log.info("User 01_Register - Step 08: Click on Register button");
+		registerPage.clickToRegisterButton();
+
+		log.info("User 01_Register - Step 09: Verify register successfully");
+		verifyTrue(registerPage.isSuccessMessageDisplayed());
+
+		log.info("User 01_Register - Step 10: Click on 'Logout' button");
+		homePage = registerPage.clickToLogOutButton();
+
+	}
+
+	@Test
+	public void TC_02_Login_To_System() {
+		log.info("User 02_Login - Step 01: Click on 'Login' Link");
+		loginPage = homePage.clickToLoginButton();
+
+		log.info("User 02_Login - Step 02: Input to 'Email' textbox:" + email );
+		loginPage.inputEmailTextBox(email);
+
+		log.info("User 02_Login - Step 03: Input to 'Password' textbox:" + password );
+		loginPage.inputPasswordTextBox(password);
+
+		log.info("User 02_Login - Step 04: Click on 'Login' button");
+		homePage = loginPage.clickToLoginButton();
+ 
+		log.info("User 02_Login - Step 09: verify login successfully");
+		verifyTrue(homePage.isHomePageSliderDisplayed());
+	}
+
+	@AfterClass
+	public void afterClass() {
+		log.info("Post-Condition: Close browser");
+		driver.quit();
+	}
+
+	HomePageObject homePage;
+	LoginPageObject loginPage;
+	RegisterPageObject registerPage;
+	MyAccountPageObject myAccountPage;
+
+}
